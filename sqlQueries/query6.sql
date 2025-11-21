35e7d2f70486ff6db0103ac62d63484d975a0004
halina <halina.maksimenka@innowise.com>
2025-11-21 12:12:32 +0300

    sql_queries

---
+++ sqlQueries/query6.sql
@@ -0,0 +1,14 @@
+-- Output cities with the number of active and inactive customers 
+-- Sort by the number of inactive customers in descending order.
+
+SELECT DISTINCT c.city 
+	 , COUNT(cust.active) FILTER (WHERE cust.active = 1)
+	   OVER(PARTITION BY c.city) "active_cust"
+	 , COUNT(cust.active) FILTER (WHERE cust.active <> 1)
+	   OVER(PARTITION BY c.city) "inactive_cust"
+FROM   city c
+JOIN   address a
+  ON   c.city_id = a.city_id
+JOIN   customer cust
+  ON   a.address_id = cust.address_id
+ORDER BY inactive_cust DESC
---
+++ sqlQueries/query7.sql
@@ -0,0 +1,57 @@
+-- Output the category of movies that have the highest number of total rental hours 
+-- in the city (customer.address_id in this city) 
+-- and that start with the letter “a”. 
+-- Do the same for cities that have a “-” in them. 
+-- Write everything in one query.
+
+(
+SELECT c.city
+     , cut.name as "category"
+     , SUM(EXTRACT(epoch FROM r.return_date - r.rental_date)/3600) as "total_rental_hours"
+FROM   customer cust
+JOIN   rental r
+  ON   cust.customer_id = r.customer_id
+JOIN   address a
+  ON   cust.address_id = a.address_id
+JOIN   city c
+  ON   a.city_id = c.city_id
+JOIN   inventory i
+  ON   r.inventory_id = i.inventory_id
+JOIN   film f
+  ON   i.film_id = f.film_id
+JOIN   film_category fc
+  ON   f.film_id = fc.film_id
+JOIN   category cut
+  ON   fc.category_id = cut.category_id
+GROUP BY 1, 2
+HAVING c.city LIKE 'a%'
+AND    SUM(EXTRACT(epoch FROM r.return_date - r.rental_date)/3600) IS NOT NULL
+ORDER BY 3 DESC
+LIMIT 1
+)
+UNION
+(
+SELECT c.city
+     , cut.name as "category"
+     , SUM(EXTRACT(epoch FROM r.return_date - r.rental_date)/3600) as "total_rental_hours"
+FROM   customer cust
+JOIN   rental r
+  ON   cust.customer_id = r.customer_id
+JOIN   address a
+  ON   cust.address_id = a.address_id
+JOIN   city c
+  ON   a.city_id = c.city_id
+JOIN   inventory i
+  ON   r.inventory_id = i.inventory_id
+JOIN   film f
+  ON   i.film_id = f.film_id
+JOIN   film_category fc
+  ON   f.film_id = fc.film_id
+JOIN   category cut
+  ON   fc.category_id = cut.category_id
+GROUP BY 1, 2
+HAVING c.city LIKE '%-%'
+AND    SUM(EXTRACT(epoch FROM r.return_date - r.rental_date)/3600) IS NOT NULL
+ORDER BY 3 DESC
+LIMIT 1
+)
